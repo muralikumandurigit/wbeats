a26571f317a22f38ee002b8386ccc113d06af226
ADMIN <ADMIN@DESKTOP-KVNE31C>
2020-02-19 23:15:22 +0530

    renaming mysql to database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var db = require('util/database');
+var pool = db.getPool();
+
 var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000;
@@ -5,3 +8,17 @@ app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
+
+module.exports = function(app, express) {
+	var api = express.Router();
+	
+	api.get	('/students', () => {
+		var query = 'select * from student_admissions';
+		pool.getConnection((err, connection) => {
+			connection.query(query, (err, rows) => {
+				res.json({type : "success", code : 200, data : rows});
+				connection.release;
+			})
+		});
+	});
+}
