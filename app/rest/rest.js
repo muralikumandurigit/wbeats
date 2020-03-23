74b51df8ef39abf5a7fa428377a1e4ce101eb853
ADMIN <muralikumanduri@gmail.com>
2020-03-23 18:48:16 +0530

    Now ui displaying table data...

--- app/rest/rest.js
+++ app/rest/rest.js
@@ -1,17 +1,23 @@
 var students = require('../controller/students');
 var app = global.app;
+
+var sendResponse = function(res, data) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.send(data);
+}
+
 app.get('/students', (req, res) => {
 	console.log('Request came to students...');
     students.getAllStudents((students) => {
 	console.log(students);
-	res.send(students);
+	sendResponse(res, students);
    });
 });
 
 app.post('/student/register', (req, res) => {
 	console.log("Received request for post");
 	students.newStudent(req.body, (studentId) => {
-		res.send("New Student saved successfully with student id : " + studentId);
+		sendResponse(res, "New Student saved successfully with student id : " + studentId);
 	});;
 });
 
